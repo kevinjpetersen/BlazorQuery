1bfc1bfccb672c775dfab8a04f47752868290e9f
Kevin J. Petersen <kevingeeken@gmail.com>
2019-07-09 04:25:00 +0200

    Refactored, Added NuGet Package

--- src/BlazorQuery.TestApp/Startup.cs
+++ src/BlazorQuery.TestApp/Startup.cs
@@ -26,9 +26,9 @@ namespace BlazorQueryApp.TestApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddBlazorQuery();
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddBlazorQuery();
             services.AddSingleton<WeatherForecastService>();
         }
 
